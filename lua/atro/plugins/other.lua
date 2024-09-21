9ddb07803ec74929eac8e246b66049e2487622c5
atropos <sv7n@pm.me>
2024-09-21 20:39:54 +0100

    Backtracking on some changes.

--- lua/atro/plugins/other.lua
+++ lua/atro/plugins/other.lua
@@ -76,21 +76,42 @@ local plugins = {
 	{
 		"karb94/neoscroll.nvim",
 		config = function()
-			require("neoscroll").setup({})
+			local neoscroll = require("neoscroll")
+			neoscroll.setup({})
 
-			local t = {}
-			-- Syntax: t[keys] = {function, {function arguments}}
-			t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "150" } }
-			t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "150" } }
-			t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "250" } }
-			t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "250" } }
-			t["<C-y>"] = { "scroll", { "-0.10", "false", "30" } }
-			t["<C-e>"] = { "scroll", { "0.10", "false", "30" } }
-			t["zt"] = { "zt", { "150" } }
-			t["zz"] = { "zz", { "150" } }
-			t["zb"] = { "zb", { "150" } }
-
-			require("neoscroll.config").set_mappings(t)
+			local keymap = {
+				["<C-u>"] = function()
+					neoscroll.ctrl_u({ duration = 150 })
+				end,
+				["<C-d>"] = function()
+					neoscroll.ctrl_d({ duration = 150 })
+				end,
+				["<C-b>"] = function()
+					neoscroll.ctrl_b({ duration = 250 })
+				end,
+				["<C-f>"] = function()
+					neoscroll.ctrl_f({ duration = 250 })
+				end,
+				["<C-y>"] = function()
+					neoscroll.scroll(-0.1, { move_cursor = false, duration = 30 })
+				end,
+				["<C-e>"] = function()
+					neoscroll.scroll(0.1, { move_cursor = false, duration = 30 })
+				end,
+				["zt"] = function()
+					neoscroll.zt({ half_win_duration = 150 })
+				end,
+				["zz"] = function()
+					neoscroll.zz({ half_win_duration = 150 })
+				end,
+				["zb"] = function()
+					neoscroll.zb({ half_win_duration = 150 })
+				end,
+			}
+			local modes = { "n", "v", "x" }
+			for key, func in pairs(keymap) do
+				vim.keymap.set(modes, key, func)
+			end
 		end,
 	},
 	{
