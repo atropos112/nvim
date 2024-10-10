8e0e57fdf7b9a6ee1ac39fef62b125b22dc1f067
atropos <sv7n@pm.me>
2024-10-10 08:27:38 +0100

    Added dap for excepition breakpoints.

--- lua/atro/dap/plugins.lua
+++ lua/atro/dap/plugins.lua
@@ -107,6 +107,16 @@ M.dap_plugins_and_load = function()
 			end,
 		},
 		{
+			"lucaSartore/nvim-dap-exception-breakpoints",
+			dependencies = { "mfussenegger/nvim-dap" },
+
+			config = function()
+				local set_exception_breakpoints = require("nvim-dap-exception-breakpoints")
+
+				vim.api.nvim_set_keymap("n", "<leader>dy", "", { desc = "[D]ebug [C]ondition breakpoints", callback = set_exception_breakpoints })
+			end,
+		},
+		{
 			"rcarriga/nvim-dap-ui",
 			dependencies = {
 				"mfussenegger/nvim-dap",
