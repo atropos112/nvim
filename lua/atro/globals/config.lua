1d0a13d1a4fd80a901822ff29a864016cdb29715
atropos <sv7n@pm.me>
2025-01-18 08:52:32 +0000

    harper ls not quiet there yet.

--- lua/atro/globals/config.lua
+++ lua/atro/globals/config.lua
@@ -9,34 +9,34 @@ return {
 	global_linters = { "codespell" },
 	global_lsps = {
 		-- INFO: Not sure how to use the dict yet.
-		harper_ls = {
-			settings = {
-				["harper-ls"] = {
-					codeActions = {
-						forceStable = true,
-					},
-					userDictPath = vim.fn.stdpath("config") .. "/dict/user_dict.txt",
-					linters = {
-						spell_check = true,
-						spelled_numbers = false,
-						an_a = true,
-						sentence_capitalization = false, -- otherwise makes ~/.local -> ~/.Local suggestions.
-						unclosed_quotes = true,
-						wrong_quotes = false,
-						long_sentences = true,
-						repeated_words = true,
-						spaces = true,
-						matcher = true,
-						correct_number_suffix = true,
-						number_suffix_capitalization = true,
-						multiple_sequential_pronouns = true,
-						linking_verbs = false,
-						avoid_curses = true,
-						terminating_conjunctions = true,
-					},
-				},
-			},
-		},
+		-- harper_ls = {
+		-- 	settings = {
+		-- 		["harper-ls"] = {
+		-- 			codeActions = {
+		-- 				forceStable = true,
+		-- 			},
+		-- 			userDictPath = vim.fn.stdpath("config") .. "/dict/user_dict.txt",
+		-- 			linters = {
+		-- 				spell_check = true,
+		-- 				spelled_numbers = false,
+		-- 				an_a = true,
+		-- 				sentence_capitalization = false, -- otherwise makes ~/.local -> ~/.Local suggestions.
+		-- 				unclosed_quotes = true,
+		-- 				wrong_quotes = false,
+		-- 				long_sentences = true,
+		-- 				repeated_words = true,
+		-- 				spaces = true,
+		-- 				matcher = true,
+		-- 				correct_number_suffix = true,
+		-- 				number_suffix_capitalization = true,
+		-- 				multiple_sequential_pronouns = true,
+		-- 				linking_verbs = false,
+		-- 				avoid_curses = true,
+		-- 				terminating_conjunctions = true,
+		-- 			},
+		-- 		},
+		-- 	},
+		-- },
 	},
 	languages = {
 		csv = {},
