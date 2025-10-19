cff7a87378b4926a96ee56f9c53df4a0f56fc6fe
atropos <sv7n@pm.me>
2025-10-19 20:11:42 +0100

    Deleted some go plugins i no longer use and added some nice comments to ones that remain.

--- lua/atro/plugins/lang_specific/go.lua
+++ lua/atro/plugins/lang_specific/go.lua
@@ -1,47 +1,12 @@
 if CONFIG.languages["go"] then
 	return {
-		-- INFO: Can use fatih/vim-go or ray-x/go.nvim but they both seem unecessarily bloated
-		-- This plugin has also (somehow) figured out how to run debug tests for ginkgo and go test
-		-- With basically zero setup. And get to use my own LSP configs so renames etc work.
-		{
-			"olexsmir/gopher.nvim",
-			ft = "go",
-			dependencies = {
-				"nvim-lua/plenary.nvim",
-				"nvim-treesitter/nvim-treesitter",
-				"mfussenegger/nvim-dap", -- (optional) only if you use `gopher.dap`
-			},
-			config = function()
-				require("atro.mason").ensure_installed({
-					"gomodifytags",
-					"gotestsum", -- For neotest-golang
-					"gotests",
-					"impl",
-					"iferr",
-					-- "dlv", -- INFO: as a dap package it already gets installed
-				})
-
-				-- TODO: Add bunch of keymaps for go-specific commands look at
-				-- https://github.com/olexsmir/gopher.nvim to see what commands are available
-				-- from the plugin itself.
-
-				require("gopher").setup(
-					---@type gopher.Config
-					{}
-				)
-			end,
-		},
-		-- Shows the "implemented by: " on each interface to show which structs implement it
-		-- Also shows "implements: " on each struct to show which interfaces it implements
-		{
-			"maxandron/goplements.nvim",
-			ft = "go",
-			opts = {},
-		},
+		-- Section: deep-completion source for unimported packages. Completes unimported packages as you type which go for some reason does not do by default.
 		{
 			"samiulsami/cmp-go-deep",
 			dependencies = { "kkharji/sqlite.lua" },
 		},
+		-- Section: Providing configurations for launching go debugger (delve) and debugging individual tests.
+		-- Looks like bunch of nvim-dap configurations that can be done manually but this plugin does it for you.
 		{
 			"leoluz/nvim-dap-go",
 			ft = "go",
